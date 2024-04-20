66e88ec0fd640996ede7855539b334a1b87bd41a
JuanjoMateu <juanjo_mateu@hotmail.es>
2024-04-20 21:02:08 +0200

    CRUD tasks completes

--- controllers/TaskController.js
+++ controllers/TaskController.js
@@ -23,14 +23,51 @@ const TaskController = {
             res.send(`
                 <h1>Todas las tareas</h1>
                 ${task.map(task => {
-                    return (
-                        `<div>
+                return (
+                    `<div>
                         <h2>${task.title}</h2>
                         <p>${task.completed}</p>
                         </div>`
-                    )
-                }).join('')}
+                )
+            }).join('')}
             `)
+        } catch (error) {
+            console.log(error)
+        }
+    },
+    async getByID(req, res) {
+        try {
+            const id = req.params._id
+            const task = await Task.findById(id)
+            res.json(task)
+        } catch (error) {
+            console.log(error)
+        }
+    },
+    async deleteTask(req, res) {
+        try {
+            const id = req.params._id
+            const deleteTask = await Task.findByIdAndDelete(id)
+            if (!deleteTask) {
+                return res.status(404).json({ mensaje: "Task not exist" })
+            }
+            res.json({ mensaje: "Task deleted", deleteTask })
+        } catch (error) {
+            console.log(error)
+        }
+    },
+    async updateCompleted(req, res) {
+        try {
+            const id = req.params._id
+            const updateTask = await Task.findByIdAndUpdate(
+                id, {
+                completed: true
+            }, { new: true }
+            )
+            if (!updateTask) {
+                return res.status(404).json({ mensaje: 'Task not founded' })
+            }
+            res.json(updateTask)
         } catch (error) {
             console.log(error)
         }
--- routes/task.js
+++ routes/task.js
@@ -6,6 +6,9 @@ const TaskController = require('../controllers/TaskController')
 router.post('/create', TaskController.create)
 router.get('/', TaskController.getAll)
 router.get('/getAll', TaskController.getAllSSR)
+router.get('/id/:_id', TaskController.getByID)
+router.delete('/id/:_id', TaskController.deleteTask)
+router.put('/markAsCompleted/:_id', TaskController.updateCompleted)
 
 
 
