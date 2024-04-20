afda49e49cf5a255f9bf48b81fa83cfcabe2aaa8
JuanjoMateu <juanjo_mateu@hotmail.es>
2024-04-20 19:22:01 +0200

    mongo uri created

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express()
-const PORT = 3000
+require('dotenv').config()
+PORT = process.env. PORT || 4000
 
 app.get('/', (req, res) => (res.send('Proyecto BACK')))
 
